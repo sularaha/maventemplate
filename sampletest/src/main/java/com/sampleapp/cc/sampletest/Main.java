e45f6c7e156bf91c9b10802695a8cfc049eae041
sularaha <46780265+sularaha@users.noreply.github.com>
2019-01-17 14:18:03 +0200

    Update Main.java

--- sampletest/src/main/java/com/sampleapp/cc/sampletest/Main.java
+++ sampletest/src/main/java/com/sampleapp/cc/sampletest/Main.java
@@ -7,8 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         
-        Lena Kaka
-        
         String str = null;
         if (StringUtils.isEmpty(str)) {
             System.out.println("------- Success!!!");
